868cb046d9f6fdaf4447730bbb0fff5e5445a434
Pedro Rafael Santos <psantos@eneida.io>
2024-07-03 20:45:28 +0100

    small fix

--- src/views/admin/vehicles/views/utils/Utils.ts
+++ src/views/admin/vehicles/views/utils/Utils.ts
@@ -9,5 +9,6 @@ const convertWhiteSpaceToNull = (value: string | null): string | null =>
 export const verifyWhiteSpaces = (vehicle: IVehicle): IVehicle => {
   vehicle.version = convertWhiteSpaceToNull(vehicle.version);
   vehicle.color = convertWhiteSpaceToNull(vehicle.color);
+  vehicle.observations = convertWhiteSpaceToNull(vehicle.observations);
   return vehicle;
 };
