64a21c9dbaa91c7cd32d6f599932fa5c071ef7d2
Pedro Rafael Santos <psantos@eneida.io>
2024-09-07 00:55:53 +0100

    small fix

---
+++ src/views/admin/client-messages/views/components/columns/ClientMessageColumns.tsx
@@ -0,0 +1,88 @@
+import { GridColDef, GridRenderCellParams } from '@mui/x-data-grid';
+import Actions from '../../../../../../components/table/utils/Actions';
+import { RouteName } from '../../../../../../models/enums/RouteType';
+import { MdCheckCircle, MdOutlineError } from 'react-icons/md';
+import { Status } from '../../../models/enums/StatusEnum';
+
+export default function columns(refetch: () => void): GridColDef[] {
+  return [
+    {
+      field: 'id',
+      headerName: '#',
+      width: 50
+    },
+    {
+      field: 'name',
+      headerName: 'Nome',
+      width: 200
+    },
+    {
+      field: 'email',
+      headerName: 'Email',
+      width: 250
+    },
+    {
+      field: 'phoneNumber',
+      headerName: 'Contacto',
+      width: 120
+    },
+    {
+      field: 'createdDate',
+      headerName: 'Data/Hora',
+      width: 150,
+      type: 'dateTime',
+      valueFormatter: (params) =>
+        `${(params.value as Date).toLocaleDateString()} ${(
+          params.value as Date
+        ).toLocaleTimeString()}`
+    },
+    {
+      field: 'status',
+      headerName: 'Estado',
+      width: 100,
+      type: 'singleSelect',
+      valueOptions: () => [
+        {
+          value: 1,
+          label: 'Aberto'
+        },
+        {
+          value: 2,
+          label: 'Fechado'
+        }
+      ],
+      renderCell: (params: GridRenderCellParams<any, Date>) => (
+        <div className="flex items-center">
+          {(params.value as unknown as Status) === Status.OPEN ? (
+            <>
+              <MdOutlineError className="me-2 text-amber-500 dark:text-amber-300" />
+              {'Aberto'}
+            </>
+          ) : (
+            <>
+              <MdCheckCircle className="me-2 text-green-500 dark:text-green-300" />
+              {'Fechado'}
+            </>
+          )}
+        </div>
+      )
+    },
+    {
+      field: 'actions',
+      headerName: 'Ações',
+      width: 100,
+      sortable: false,
+      filterable: false,
+      disableExport: true,
+      renderCell: (params: GridRenderCellParams<any, Date>) => (
+        <Actions
+          editTitle={'Editar mensagem'}
+          deleteTitle={'Apagar mensagem'}
+          routeName={RouteName.CLIENT_MESSAGES}
+          id={Number(params.id)}
+          refetch={refetch}
+        />
+      )
+    }
+  ];
+}
